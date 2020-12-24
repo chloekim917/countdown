26f12f3ffd8cc0bf1ca1dc4d958dd302c713123d
chloekim917 <chloekim917@gmail.com>
2020-12-24 18:03:10 -0500

    add zero in front of single min

--- countdown.js
+++ countdown.js
@@ -6,9 +6,10 @@ const countdown = document.getElementById('countdown');
 setInterval(updateCountdown, 1000);
 
 function updateCountdown(){
-    const mins = Math.floor(time / 60);
+    let mins = Math.floor(time / 60);
     let seconds = time % 60;
 
+    mins = mins < 10 ? '0' + mins : mins;
     seconds = seconds < 10 ? '0' + seconds : seconds;
 
     countdown.innerHTML = `${mins} : ${seconds}`;
